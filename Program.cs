beced73ec9c9a84631a4bdb1bdc9c2e4bc7c7291
Alexander Ulyanitsky <AlexanderUlyanitsky@coherentsolutions.com>
2021-08-18 11:51:15 +0300

    Changes

--- Program.cs
+++ Program.cs
@@ -1,34 +1,10 @@
-﻿using DigitalCatalogue;
-using System;
-
-namespace DigitalCatalog
+﻿namespace DigitalCatalog
 {
     class Program
     {
         static void Main(string[] args)
         {
-			var richterBook = new Book("1234567891011", "CLR Via C#", DateTime.Today);
-			var troelsenBook = new Book("1234567891012", "Pro C# 9 with .NET 5", DateTime.Today);
-
-
-			var catalog = new Catalog();
-
-			catalog[richterBook.ISBN] = richterBook;
-			catalog[troelsenBook.ISBN] = troelsenBook;
-
-			Console.WriteLine("Check Setter:\n");
-			foreach (Book book in catalog)
-			{
-				Console.WriteLine(
-					$"\tISBN = {book.ISBN};\n\tBookName = {book.BookName};\n\tPublication date = {book.PublicationDate}\n");
-			}
-
-			Console.WriteLine("--------------------------------------------------------------");
-			var rBook = catalog[richterBook.ISBN];
-			Console.WriteLine("Check Getter:\n");
-			Console.WriteLine($"\tISBN = {rBook.ISBN};\n\tBookName = {rBook.BookName};\n\tPublication date = {rBook.PublicationDate}");
-
-			Console.ReadKey();
+			
 		}
     }
 }
